5e95449b16a43f8448fd1205f4c55d1757e42e3b
dhairya218 <dhairykdandaiya@gmail.com>
2025-05-08 13:33:44 +0530

    fix: add webpack alias for @ path

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 };
 
 module.exports = nextConfig;
